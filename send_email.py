989e164f9034f2010228c1f7b337be262cfecdac
Ivan Maslennikov <roma.axe.99@gmail.com>
2024-02-05 20:36:28 +0300

    Initial commit
    script for sending email

---
+++ send_email.py
@@ -0,0 +1,36 @@
+
+
+import smtplib
+import time 
+
+# Параметры подключения к серверу
+smtp_server = "smtp.gmail.com"  # Адрес SMTP сервера
+smtp_port = 587                   # Порт, использующийся для TLS/STARTTLS
+sender_email = 'roma.axe.99@gmail.com'  # Адрес отправителя
+receiver_email = 'origino@bk.ru'  # Адрес получателя
+password = 'zgsteotlcaktbknb'             # Пароль отправителя
+
+# Текст сообщения
+message = f"""\
+Subject: Hi there
+try:
+tes_client
+@ivan maslennikov {time.time()}"""
+
+# Создание соединения с сервером и отправка сообщения
+try:
+    # Cоединение с сервером
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()  # Установка безопасного соединения
+    server.login(sender_email, password)
+    
+    # Отправка сообщения
+
+    server.sendmail(sender_email, receiver_email, message)
+
+    
+    print("Email successfully sent", time.time())
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    server.quit()
